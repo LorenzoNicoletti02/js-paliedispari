e7ba5e4cd042a8835105db985da6fa46d0008129
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2024-11-06 10:52:11 +0100

    feat. completamento

--- pariEdispari/script.js
+++ pariEdispari/script.js
@@ -1,3 +1,4 @@
+
 /**
  * Description placeholder
  *
@@ -8,5 +9,21 @@
 
 function getrandomInt(min, max) {
     return Math.floor(Math.random() * (max - min +1) + min);
+}
+
+
+/**
+ * Description placeholder
+ *
+ * @param {number} numToCheck
+ * @returns {string}
+ */
+
+function evenodd (numToCheck) {
+    let result = "dispari";
+    IF (numToCheck % 2 === 0) {
+        result = "pari";
+    }
+    return result;
 }
 
