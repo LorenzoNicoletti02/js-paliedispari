488311adca1f72d0a7aafbb30da2fa454791f6ad
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2024-11-06 10:47:13 +0100

    feat: correzione e comprensione

---
+++ pariEdispari/script.js
@@ -0,0 +1,12 @@
+/**
+ * Description placeholder
+ *
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+
+function getrandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min +1) + min);
+}
+
